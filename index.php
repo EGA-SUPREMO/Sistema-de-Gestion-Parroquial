028db8323063a31d6ae1252f3b06e06b338c6c01
EGA-SUPREMO <cxiesestro@gmail.com>
2025-06-28 18:22:47 -0400

    editar index para redireccionar cuando se pone url no encontrada

--- index.php
+++ index.php
@@ -7,14 +7,29 @@ $accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'index';
 $controlador = "controlador/{$controladorNombre}.controlador.php";
 $controladorclase = ucwords($controladorNombre) . 'Controlador';
 
+try {
+    if (!file_exists($controlador)) {
+        throw new Exception("Controlador no encontrado");
+    }
 
-require_once $controlador;
+    require_once $controlador;
 
+    if (!class_exists($controladorclase)) {
+        throw new Exception("Clase del controlador no encontrada");
+    }
 
-$controladorInstanciado = new $controladorclase;
+    $controladorInstanciado = new $controladorclase;
 
+    if (!method_exists($controladorInstanciado, $accion)) {
+        throw new Exception("Método no encontrado");
+    }
 
-call_user_func([$controladorInstanciado, $accion]);
+    call_user_func([$controladorInstanciado, $accion]);
+
+} catch (Exception $e) {
+    header('Location: index.php?c=login&a=dashboard');
+    exit;
+}
 
 ?>
 
