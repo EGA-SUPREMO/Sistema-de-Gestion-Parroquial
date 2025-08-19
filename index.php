2794a0771574422655cfb99647d55e4fd92a554e
Jose Sanchez <jsanchezdiego@gmail.com>
2025-08-19 16:24:52 -0400

    Revert "sssss"
    This reverts commit 7f12ab560e8f9939ddf0d106e8fde21c2f0bd77d.

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+require_once 'modelo/cargarEnv.php';
+require_once 'modelo/conexion.php';
+
+cargarVariablesDeEntorno();
+
+$controladorNombre = strtolower(isset($_REQUEST['c']) ? $_REQUEST['c'] : 'login');
+$accion = isset($_REQUEST['a']) ? $_REQUEST['a'] : 'index';
+
+$controlador = "controlador/{$controladorNombre}.controlador.php";
+$controladorclase = ucwords($controladorNombre) . 'Controlador';
+
+try {
+    $pdo = BaseDatos::obtenerConexion($_ENV['DB_HOST'], $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS']);
+
+    if (!file_exists($controlador)) {
+        throw new Exception("Controlador no encontrado");
+    }
+
+    require_once $controlador;
+
+    if (!class_exists($controladorclase)) {
+        throw new Exception("Clase del controlador no encontrada");
+    }
+
+    $controladorInstanciado = new $controladorclase($pdo);
+
+    if (!method_exists($controladorInstanciado, $accion)) {
+        throw new Exception("Método no encontrado");
+    }
+
+    call_user_func([$controladorInstanciado, $accion]);
+
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    header('Location: index.php?c=login&a=dashboard');
+    exit;
+}
