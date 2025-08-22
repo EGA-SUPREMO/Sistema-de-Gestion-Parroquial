d4089d08a9848fb9d083c67cf0854bc51b7562a3
EGA-SUPREMO <cxiesestro@gmail.com>
2025-08-22 16:41:53 -0400

    cambios para que cuadren con leos casos de uso

--- vistas/login/dashboard.php
+++ vistas/login/dashboard.php
@@ -25,6 +25,13 @@
                         <div class="col-sm-6">
                             <a href="?c=reporte&a=index" class="card text-decoration-none text-dark h-100 lift-effect">
                                 <div class="card-body text-center">
+                                    <h5 class="card-title mb-0">Registrar intencion</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?c=reporte&a=index" class="card text-decoration-none text-dark h-100 lift-effect">
+                                <div class="card-body text-center">
                                    
                                     <h5 class="card-title mb-0">Generar Reportes</h5>
                                 </div>
