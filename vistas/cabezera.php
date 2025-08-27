7fef05d9b16178ade04ab72f162f639f3628ac59
EGA-SUPREMO <cxiesestro@gmail.com>
2025-08-27 16:20:13 -0400

    limpiar head duplicado en menu

--- vistas/cabezera.php
+++ vistas/cabezera.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion Parroquial</title>
     <link rel="stylesheet" href="./public/css/bootstrap.min.css">
     <link rel="stylesheet" href="./public/css/font-awesome.min.css">
     <link rel="stylesheet" href="./public/css/boostrap-icons.min.css">
+    <link rel="stylesheet" href="./public/css/menu.css">
     <link rel="stylesheet" href="./public/css/all.min.css">
 </head>
