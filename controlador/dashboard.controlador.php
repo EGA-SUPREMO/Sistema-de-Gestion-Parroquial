eef489ff6c50ba5f732ef0afaf66e46632c51220
EGA-SUPREMO <cxiesestro@gmail.com>
2025-09-04 09:34:12 -0400

    agregar boceto para mostrar tablas con un controlador

---
+++ controlador/dashboard.controlador.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'modelo/GestorFactory.php';
+require_once 'modelo/FuncionesComunes.php';
+
+class formularioControlador
+{
+    private $gestor;
+    private $nombreTabla;
+    private $mapaDatos = [
+        'administrador' => ['nombre_usuario', 'password'],
+    ];
+
+    public function __construct(PDO $pdo)
+    {
+        $this->nombreTabla = $_REQUEST['t'];
+        $this->gestor = GestorFactory::crearGestor($pdo, $this->nombreTabla);
+    }
+    public function mostrar()
+    {
+        include_once 'vistas/tabla.php';
+        require_once "controlador/tabla.php";
+    }
+}
