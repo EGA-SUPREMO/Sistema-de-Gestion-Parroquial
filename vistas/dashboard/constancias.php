b317d72f22d0b192c18afd2f5116eb3c4f58f1ac
EGA-SUPREMO <cxiesestro@gmail.com>
2025-09-11 10:50:35 -0400

    cambios menores a vista constnacias
    borrar head, y agregar confirmacion

--- vistas/dashboard/constancias.php
+++ vistas/dashboard/constancias.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Panel de Administración</title>
-    
-    <link rel="stylesheet" href="../public/styles.css">
-</head>
 <body>
-
-
-    <header class="bg-success text-white text-center py-3">
-        <h1 class="mb-0">Generar Constancias</h1>
-    </header>
-
+<header class="bg-success text-white text-center py-3">
+    <h1 class="mb-0">Generar Constancias</h1>
+</header>
  <main class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-8 col-lg-6">
@@ -22,38 +10,40 @@
                     <h2 class="card-title text-center text-primary fw-bold mb-4">Opciones disponibles </h2>
                     <div class="row g-3">
                         <div class="col-sm-6">
-                            <a href="?c=reporte&a=constancia_c" class="card text-decoration-none text-dark h-100 lift-effect">
-                                <div class="card-body text-center">
-                                
-                                    <h5 class="card-title mb-0">Constancia primera comunion (Incompleto)</h5>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6">
                             <a href="?c=reporte&a=fe_bautizo" class="card text-decoration-none text-dark h-100 lift-effect">
                                 <div class="card-body text-center">
-                                   
                                     <h5 class="card-title mb-0">Fe de Bautizo</h5>
                                 </div>
                             </a>
                         </div>
                         <div class="col-sm-6">
-                            <a href="?c=reporte&a=intenciones" class="card text-decoration-none text-dark h-100 lift-effect">
+                            <a href="?c=reporte&a=constancia_c" class="card text-decoration-none text-dark h-100 lift-effect">
                                 <div class="card-body text-center">
-                                    
-                                    <h5 class="card-title mb-0">Intenciones (Incompleto)</h5>
+                                    <h5 class="card-title mb-0">Constancia primera comunion</h5>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="?c=reporte&a=constancia_c" class="card text-decoration-none text-dark h-100 lift-effect">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title mb-0">Constancia confirmación</h5>
                                 </div>
                             </a>
                         </div>
                         <div class="col-sm-6">
                             <a href="?c=reporte&a=acta_matrimonio" class="card text-decoration-none text-dark h-100 lift-effect">
                                 <div class="card-body text-center">
-                                   
                                     <h5 class="card-title mb-0">Acta de matrimonio eclesiastico</h5>
                                 </div>
                             </a>
                         </div>
-                       
+                        <div class="col-sm-6">
+                            <a href="?c=reporte&a=intenciones" class="card text-decoration-none text-dark h-100 lift-effect">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title mb-0">Intenciones</h5>
+                                </div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </section>
