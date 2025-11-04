bba0f23381c447aabfd72cdd208fc8fd04869a58
EGA-SUPREMO <cxiesestro@gmail.com>
2025-11-04 18:32:37 -0400

    corregir estilo

--- modelo/GestorAdministrador.php
+++ modelo/GestorAdministrador.php
@@ -13,7 +13,8 @@ class GestorAdministrador extends GestorBase
         $this ->clase_nombre = "Administrador";
     }
 
-    public function obtenerPorNombreUsuario($nombre_usuario) {
+    public function obtenerPorNombreUsuario($nombre_usuario)
+    {
         $sql = "SELECT * FROM {$this->tabla} WHERE `nombre_usuario` = ?";
         return $this->hacerConsulta($sql, [$nombre_usuario], 'single');
     }
