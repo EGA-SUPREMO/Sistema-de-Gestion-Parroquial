c54635a84d2bc761b0d0e34314b703d1f0c77743
EGA-SUPREMO <cxiesestro@gmail.com>
2025-11-13 17:12:57 -0400

    borrrar botones que no seran usados nunca!

--- vistas/dashboard/administracion.php
+++ vistas/dashboard/administracion.php
@@ -7,13 +7,6 @@
                 <div class="card-body p-4">
                     <h2 class="card-title text-center text-primary fw-bold mb-4">Opciones del Panel</h2>
                     <div class="row g-3">
-                        <div class="col-sm-6">
-                            <a href="?c=peticiones&a=index" class="card text-decoration-none text-dark h-100 lift-effect">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title mb-0"> Consultar <br>Peticiones</h5>
-                                </div>
-                            </a>
-                        </div>
                           <div class="col-sm-6">
                             <a href="?c=panel&a=index&t=administrador" class="card text-decoration-none text-dark h-100 lift-effect">
                                 <div class="card-body text-center">
@@ -22,37 +15,9 @@
                             </a>
                         </div>
                         <div class="col-sm-6">
-                            <a href="?c=dashboard&a=administracion" class="card text-decoration-none text-dark h-100 lift-effect">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title mb-0">Editar tipos<br>de intención</h5>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="?c=panel&a=index&t=servicio" class="card text-decoration-none text-dark h-100 lift-effect">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title mb-0">Gestionar Servicios</h5>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="?c=panel&a=index&t=categoria_de_servicio" class="card text-decoration-none text-dark h-100 lift-effect">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title mb-0">Gestionar Categoría de Servicios</h5>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6">
                             <a href="?c=panel&a=index&t=sacerdote" class="card text-decoration-none text-dark h-100 lift-effect">
                                 <div class="card-body text-center">
                                     <h5 class="card-title mb-0">Gestionar Sacerdotes</h5>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="?c=panel&a=index&t=santo" class="card text-decoration-none text-dark h-100 lift-effect">
-                                <div class="card-body text-center">
-                                    <h5 class="card-title mb-0">Gestionar Santos</h5>
                                 </div>
                             </a>
                         </div>
